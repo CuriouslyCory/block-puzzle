3305afcd34a66a6c1d449f820b65db672466e3af
Cory Sougstad <cory@hau.me>
2024-09-23 09:29:33 -0700

    Clean up game board

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,18 +6,30 @@ export default async function Home() {
   const session = await getServerAuthSession();
   return (
     <main>
-      <div className="flex flex-col items-center justify-center gap-4">
-        <p className="text-center text-2xl text-white">
+      <header className="my-2 flex items-center justify-end gap-1 md:mx-4">
+        <span className="">
           {session && <span>Logged in as {session.user?.name}</span>}
-        </p>
+        </span>
         <Link
           href={session ? "/api/auth/signout" : "/api/auth/signin"}
-          className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
+          className="rounded-lg px-2 py-1 font-semibold no-underline transition hover:bg-blue-900/20"
         >
           {session ? "Sign out" : "Sign in"}
         </Link>
-      </div>
-      <Game />
+      </header>
+      {!session && (
+        <h2 className="text-center text-3xl">
+          Please{" "}
+          <Link
+            href="/api/auth/signin"
+            className="rounded-lg transition hover:bg-blue-900/20"
+          >
+            Sign-In
+          </Link>{" "}
+          To Begin
+        </h2>
+      )}
+      {session && <Game />}
     </main>
   );
 }
